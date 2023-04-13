de7dbe527765cbc4c525cbc3bc5f7f3c60eb1e65
IziumenkoViacheslav <2744097@gmail.com>
2023-04-13 21:10:19 +0200

    add local hardhat network and command

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,15 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  solidity: '0.8.18',
+  defaultNetwork: 'hardhat',
+  networks: {
+    local: {
+      url: 'http://127.0.0.1:8545/',
+      chainId: 31337,
+    },
+  },
 };
 
 export default config;
