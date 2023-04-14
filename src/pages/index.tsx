5183a05ad60d6e6b53cbfe38eb067b243f861386
IziumenkoViacheslav <2744097@gmail.com>
2023-04-14 21:01:18 +0200

    balanceOf feature added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,39 +10,49 @@ declare global {
 }
 
 export default function Home() {
+  const [contract, setContract] = useState<ethers.Contract>();
   const [address, setAddress] = useState('');
+  const [walletTo, setWalletTo] = useState('');
+  const [amount, setAmount] = useState<Number>();
+  const [wallet, setWallet] = useState('');
+  const [balance, setBalance] = useState('');
+  const [signer, setSigner] = useState<ethers.Signer>();
+
+  async function balanceOfWallet(wallet: string) {
+    const result = await contract?.balanceOf(wallet);
+    console.log({ result: Number(result) });
+    setBalance(Number(result).toString());
+
+    return Number(result);
+  }
+  async function transferToWallet() {
+    if (!contract) {
+      return null;
+    }
+
+    contract && (await contract.transfer(address, amount));
+  }
   async function connectToMetamask() {
     if (window.ethereum) {
-      console.log('connect');
       toast('connected to metamask');
       const accounts = await window.ethereum.request({
         method: 'eth_requestAccounts',
       });
-      console.log({ accounts });
       setAddress(accounts[0]);
       const provider = new ethers.providers.Web3Provider(window.ethereum);
-      console.log(await provider.getNetwork());
-      const balance = await provider.getBalance(accounts[0]);
-      console.log({ balance });
-      const balanceInEther = ethers.utils.formatEther(balance);
-      console.log({ balanceInEther });
       const contract = new ethers.Contract(
         '0x5fbdb2315678afecb367f032d93f642f64180aa3',
         MarshaPlus.abi,
         provider
       );
-      console.log({ contract });
-      console.log(await contract.address);
-      console.log(
-        Number(
-          await contract.balanceOf('0x17F6AD8Ef982297579C203069C1DbfFE4348c372')
-        )
-      );
+      setContract(contract);
+      const signer = provider.getSigner();
+      setSigner(signer);
     }
   }
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <div className='z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex'>
+    <div className='flex min-h-screen flex-col items-center justify-between p-4'>
+      <div className='flex flex-col w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex'>
         <button
           className='border-black border-2 rounded-lg p-2'
           onClick={connectToMetamask}
@@ -50,7 +60,38 @@ export default function Home() {
           connect to metamask
         </button>
         {address}
+        wallet address
+        <input
+          className='m-2 p-2'
+          type='text'
+          onChange={(e) => setWalletTo(e.target.value)}
+        />
+        amount of tokens
+        <input
+          className='m-2 p-2'
+          type='text'
+          onChange={(e) => setAmount(Number(e.target.value))}
+        />
+        <button
+          className='bg-blue-700 text-white rounded-lg p-2 m-2'
+          onClick={transferToWallet}
+        >
+          transfer
+        </button>
+        balance of wallet
+        <input
+          className='m-2 p-2'
+          type='text'
+          onChange={(e) => setWallet(e.target.value)}
+        />
+        <button
+          className='bg-blue-700 text-white p-2 rounded-lg'
+          onClick={() => balanceOfWallet(wallet)}
+        >
+          checkBalance
+        </button>
+        {balance}
       </div>
-    </main>
+    </div>
   );
 }
