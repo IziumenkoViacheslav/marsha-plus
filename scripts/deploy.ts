94b8b1ad2d42fc26d82579b8d0cd410f223ba031
IziumenkoViacheslav <2744097@gmail.com>
2023-04-14 18:00:11 +0200

    deploy for marshaPlus contract added

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,19 +1,23 @@
-import { ethers } from "hardhat";
+import { ethers } from 'hardhat';
 
 async function main() {
   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  // const unlockTime = currentTimestampInSeconds + 60;
+  const unlockTime = currentTimestampInSeconds + 9;
 
-  const lockedAmount = ethers.utils.parseEther("0.001");
+  const lockedAmount = ethers.utils.parseEther('0.001');
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  // const Lock = await ethers.getContractFactory("Lock");
+  // const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
 
-  await lock.deployed();
+  // await lock.deployed();
 
-  console.log(
-    `Lock with ${ethers.utils.formatEther(lockedAmount)}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  // console.log(
+  //   `Lock with ${ethers.utils.formatEther(lockedAmount)}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+  // );
+
+  const MarshaPlus = await ethers.getContractFactory('MarshaPlus');
+  const marshaPlus = await MarshaPlus.deploy();
 }
 
 // We recommend this pattern to be able to use async/await everywhere
