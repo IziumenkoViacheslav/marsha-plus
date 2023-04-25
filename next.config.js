1b1a53f9763cd9254db43b512e474f7a9f9eedc6
IziumenkoViacheslav <2744097@gmail.com>
2023-04-25 18:48:38 +0200

    images resolved from another site

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'static.justboil.me',
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
