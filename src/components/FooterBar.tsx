490f0980fae677ad39dbe8db3a8e33731b389b28
IziumenkoViacheslav <2744097@gmail.com>
2023-05-04 16:59:09 +0200

    year added

--- src/components/FooterBar.tsx
+++ src/components/FooterBar.tsx
@@ -7,21 +7,13 @@ type Props = {
 }
 
 export default function FooterBar({ children }: Props) {
-  const year = new Date().getFullYear()
+  // const year = new Date().getFullYear()
 
   return (
     <footer className={`py-2 px-6 ${containerMaxW}`}>
       <div className="block md:flex items-center justify-between">
         <div className="text-center md:text-left mb-6 md:mb-0">
-          {/* <b>
-            &copy;{year},{` `}
-            <a href="https://justboil.me/" rel="noreferrer" target="_blank">
-              Poco Maximiliano
-            </a>
-            .
-          </b> */}
-          {` `}
-          {children}
+          {2023} {children}
         </div>
         <div className="md:py-2">
           <a href="https://justboil.me" rel="noreferrer" target="_blank">
