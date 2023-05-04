765f116ecb8e2717adf23ba2a81f90ce45f98fb5
IziumenkoViacheslav <2744097@gmail.com>
2023-05-04 16:55:07 +0200

    template for empty page changed

--- src/components/CardBoxComponentEmpty.tsx
+++ src/components/CardBoxComponentEmpty.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CardBoxComponentEmpty = () => {
   return (
     <div className="text-center py-24 text-gray-500 dark:text-slate-400">
-      <p>Nothing&apos;s here…</p>
+      <p className="text-3xl">This page in progress…</p>
     </div>
   )
 }
