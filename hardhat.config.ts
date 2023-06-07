3a24bd884524015a8a9262f769d0cd634f58406a
IziumenkoViacheslav <2744097@gmail.com>
2023-06-07 14:48:36 +0200

    binance testnet and binance prod added for deployment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,8 @@
 import { HardhatUserConfig } from 'hardhat/config'
 import '@nomicfoundation/hardhat-toolbox'
 
+import { mnemonic } from './secret.json'
+
 const config: HardhatUserConfig = {
   solidity: '0.8.18',
   defaultNetwork: 'hardhat',
@@ -10,6 +12,18 @@ const config: HardhatUserConfig = {
       chainId: 31337,
       allowUnlimitedContractSize: true,
       timeout: 1800000,
+    },
+    testnet: {
+      url: 'https://data-seed-prebsc-1-s1.binance.org:8545',
+      chainId: 97,
+      gasPrice: 20000000000,
+      accounts: { mnemonic: mnemonic },
+    },
+    mainnet: {
+      url: 'https://bsc-dataseed.binance.org/',
+      chainId: 56,
+      gasPrice: 20000000000,
+      accounts: { mnemonic: mnemonic },
     },
   },
 }
