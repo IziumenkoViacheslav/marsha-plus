7eb937ca7e0a67bd4c3c0da5e1fd63ecb4236664
IziumenkoViacheslav <2744097@gmail.com>
2023-06-07 16:28:15 +0200

    check balance of deployer added to deploy

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,28 +1,36 @@
-import { ethers } from 'hardhat';
+import { ethers } from 'hardhat'
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  // const currentTimestampInSeconds = Math.round(Date.now() / 1000)
   // const unlockTime = currentTimestampInSeconds + 60;
-  const unlockTime = currentTimestampInSeconds + 9;
+  // const unlockTime = currentTimestampInSeconds + 9
 
-  const lockedAmount = ethers.utils.parseEther('0.001');
+  // const lockedAmount = ethers.utils.parseEther('0.001')
 
-  // const Lock = await ethers.getContractFactory("Lock");
-  // const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  const MarshaPlus = await ethers.getContractFactory('MarshaPlus')
+  const gasPrice = await MarshaPlus.signer.getGasPrice()
+  console.log(`Current gas price: ${gasPrice}`)
 
-  // await lock.deployed();
+  const estimatedGas = await MarshaPlus.signer.estimateGas(MarshaPlus.getDeployTransaction())
+  console.log(`Estimated gas: ${estimatedGas}`)
 
-  // console.log(
-  //   `Lock with ${ethers.utils.formatEther(lockedAmount)}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  // );
-
-  const MarshaPlus = await ethers.getContractFactory('MarshaPlus');
-  const marshaPlus = await MarshaPlus.deploy();
+  const deploymentPrice = gasPrice.mul(estimatedGas)
+  const deployerBalance = await MarshaPlus.signer.getBalance()
+  console.log(`Deployer balance:  ${ethers.utils.formatEther(deployerBalance)}`)
+  console.log(`Deployment price:  ${ethers.utils.formatEther(deploymentPrice)}`)
+  if (deployerBalance.lt(deploymentPrice)) {
+    throw new Error(
+      `Insufficient funds. Top up your account balance by ${ethers.utils.formatEther(
+        deploymentPrice.sub(deployerBalance)
+      )}`
+    )
+  }
+  await MarshaPlus.deploy()
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+  console.error(error)
+  process.exitCode = 1
+})
