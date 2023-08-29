a0dd5c84bbdbb0577c28cf020169b68f1dc060d1
IziumenkoViacheslav <2744097@gmail.com>
2023-08-29 18:39:24 +0200

    refactor

--- contracts/NFT-Marsha.sol
+++ contracts/NFT-Marsha.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8;
 
+import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol'
 import '@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
 import '@openzeppelin/contracts/utils/Counters.sol';
 
