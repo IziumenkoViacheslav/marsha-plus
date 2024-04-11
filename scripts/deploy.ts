642b3459b12b439fbfba8d52a4b668cbd0a1ec83
IziumenkoViacheslav <2744097@gmail.com>
2024-04-11 21:24:54 +0200

    deploy script commented for build

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,36 +1,38 @@
-import { ethers } from 'hardhat'
+// import { ethers } from 'hardhat'
 
-async function main() {
-  // const currentTimestampInSeconds = Math.round(Date.now() / 1000)
-  // const unlockTime = currentTimestampInSeconds + 60;
-  // const unlockTime = currentTimestampInSeconds + 9
+// async function main() {
+//   // const currentTimestampInSeconds = Math.round(Date.now() / 1000)
+//   // const unlockTime = currentTimestampInSeconds + 60;
+//   // const unlockTime = currentTimestampInSeconds + 9
 
-  // const lockedAmount = ethers.utils.parseEther('0.001')
+//   // const lockedAmount = ethers.utils.parseEther('0.001')
 
-  const MarshaPlus = await ethers.getContractFactory('MarshaPlus')
-  const gasPrice = await MarshaPlus.signer.getGasPrice()
-  console.log(`Current gas price: ${gasPrice}`)
+//   const MarshaPlus = await ethers.getContractFactory('MarshaPlus')
+//   const gasPrice = await MarshaPlus.signer.getGasPrice()
+//   console.log(`Current gas price: ${gasPrice}`)
 
-  const estimatedGas = await MarshaPlus.signer.estimateGas(MarshaPlus.getDeployTransaction())
-  console.log(`Estimated gas: ${estimatedGas}`)
+//   const estimatedGas = await MarshaPlus.signer.estimateGas(MarshaPlus.getDeployTransaction())
+//   console.log(`Estimated gas: ${estimatedGas}`)
 
-  const deploymentPrice = gasPrice.mul(estimatedGas)
-  const deployerBalance = await MarshaPlus.signer.getBalance()
-  console.log(`Deployer balance:  ${ethers.utils.formatEther(deployerBalance)}`)
-  console.log(`Deployment price:  ${ethers.utils.formatEther(deploymentPrice)}`)
-  if (deployerBalance.lt(deploymentPrice)) {
-    throw new Error(
-      `Insufficient funds. Top up your account balance by ${ethers.utils.formatEther(
-        deploymentPrice.sub(deployerBalance)
-      )}`
-    )
-  }
-  await MarshaPlus.deploy()
-}
+//   const deploymentPrice = gasPrice.mul(estimatedGas)
+//   const deployerBalance = await MarshaPlus.signer.getBalance()
+//   console.log(`Deployer balance:  ${ethers.utils.formatEther(deployerBalance)}`)
+//   console.log(`Deployment price:  ${ethers.utils.formatEther(deploymentPrice)}`)
+//   if (deployerBalance.lt(deploymentPrice)) {
+//     throw new Error(
+//       `Insufficient funds. Top up your account balance by ${ethers.utils.formatEther(
+//         deploymentPrice.sub(deployerBalance)
+//       )}`
+//     )
+//   }
+//   await MarshaPlus.deploy()
+// }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error)
-  process.exitCode = 1
-})
+// // We recommend this pattern to be able to use async/await everywhere
+// // and properly handle errors.
+// main().catch((error) => {
+//   console.error(error)
+//   process.exitCode = 1
+// })
+
+export {}
